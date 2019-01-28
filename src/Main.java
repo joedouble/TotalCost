c96424926d15e8664a4e37a3334a4c846b200f1f
Joseph Double <josephdouble@Josephs-MacBook-Pro.local>
2019-01-28 09:55:58 -0500

    Adding Scanner to program to allow user input.

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         double salesTax = 0.06;
-        double itemPrice = 200.51;
-        double totalCost = itemPrice + (salesTax * itemPrice);
+        double itemPrice = 0;
+        double totalCost = 0;
+
+        Scanner myScanner = new Scanner(System.in);
+
+        System.out.println("What is the price of the item you'd like to buy?");
+        itemPrice = myScanner.nextInt();
+
+        totalCost = itemPrice + (salesTax * itemPrice);
+
+        System.out.print("The total cost of the item is $" + totalCost + ". ");
 
         if (totalCost <= 50) {
             System.out.println("Go for it!");
         } else if (totalCost > 50 & totalCost <= 100) {
-            System.out.println("Are you sure about this?");
-        } else {
+            System.out.println("Are you sure about this purchase?");
+        } else if (totalCost > 100){
             System.out.println("I'm hiding your credit card.");
         }
     }
